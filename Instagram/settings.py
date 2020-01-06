ef11ea9c4d7b6b9d0a447c25d28c5a397e298d0a
Dancanoduor <dancanoduor1990@gmail>
2020-01-06 09:04:12 +0300

    modify home

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -59,8 +59,8 @@ ROOT_URLCONF = 'Instagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'DIRS': [ 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # 'DIRS': [ 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
